e3a84307ccd32f74a3eb3ead651af78178852068
Start Coding Student 1 <session1@START-Coding-MBP-09.local>
2016-01-29 15:18:16 -0700

    1st commit - andy. Page Template 90% finished

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>DieWing Product Index</title>
+    <link rel="stylesheet" type="text/css" href="primaryStylesheet.css">
+</head>
+<body>
+<!--###  body start  ###-->
+<!--# # # # # # # # # #-->
+<!--#   HEADER DIV    #-->
+<!--# # # # # # # # # #-->
+<header>
+    <div id="head">
+        <div id="siteName">
+            <h1>DieWing</h1>
+        </div>
+        <br>
+        <div id="signIn">
+            <ul>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="signIn.php">Sign In</a></li>
+            </ul>
+        </div>
+    </div>
+    <br><br><br>
+    <div id="navbar">
+        <ul>
+            <li><a href="FAQ.php">FAQ</a></li>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li style="float: left; margin-left: 7%"><p>Look Fly, Friend</p></li>
+        </ul>
+    </div>
+</header>
+<!--***  header end  ***-->
+<!--# # # # # # # # # # -->
+<!--#   CONTENT DIV   #-->
+<!--# # # # # # # # # # -->
+<div id="content">
+    <img src="http://yaleherald.com/wp-content/uploads/2011/04/BTbw-550x269.jpg">
+</div>
+<!--***  content end  ***-->
+<!--# # # # # # # # # #-->
+<!--#   FOOTER DIV    #-->
+<!--# # # # # # # # # #-->
+<footer>
+    <div id="copyright">
+        <p>&copy 2015 - DieWing Inc. All Rights Reserved</p>
+    </div>
+    <div id="lowLinks">
+        <a href="index.php">Home</a>
+        <a href="signIn.php">Sign-In</a>
+        <a href="about.php">About Us</a>
+    </div>
+</footer>
+<!--***  footer end   ***-->
+<!--***    body end   ***-->
+</body>
+</html>
