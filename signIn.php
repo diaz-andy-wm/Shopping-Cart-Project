5ddca228598a5a4e26292ef454a06682c2dec359
Start Coding Student 1 <session1@START-Coding-MBP-09.local>
2016-02-05 15:00:00 -0700

    finished sign-in page (basically) - andy

--- signIn.php
+++ signIn.php
@@ -9,37 +9,132 @@
 <!--# # # # # # # # # #-->
 <!--#   HEADER DIV    #-->
 <!--# # # # # # # # # #-->
-<header>
-    <div id="head">
-        <div id="siteName">
-            <h1>DieWing</h1>
-            <img src="pictures/DW-logo(tan).png">
-        </div>
-        <br>
-        <div id="signIn">
-            <ul>
-                <li><a href="profile.php">Profile</a></li>
-                <li><a href="signIn.php">Sign In</a></li>
-            </ul>
-        </div>
-    </div>
-    <br><br><br>
-    <div id="navbar">
-        <ul>
-            <li style="float: left; margin-left: 7%"><p>Look Fly, Friend</p></li>
-            <li><a href="FAQ.php">FAQ</a></li>
-            <li><a href="products.php">Products</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="index.php">Home</a></li>
-        </ul>
-    </div>
-</header>
+<div id="signInName">
+    <a href="index.php"><img src="pictures/DieWing_card"></a>
+</div>
 <!--***  header end  ***-->
 <!--# # # # # # # # # # -->
 <!--#   CONTENT DIV   #-->
 <!--# # # # # # # # # # -->
-<div id="content">
-    <img src="http://yaleherald.com/wp-content/uploads/2011/04/BTbw-550x269.jpg">
+<div id="signInContent">
+<h3>Log-in</h3>
+    <form id="logIn">
+        <label for="logInUsername">Username: </label>
+        <input type="text" class="infoSlot" name="logInsername" id="logInUsername"><br>
+
+        <label for="Password">Password: </label>
+        <input type="password" class="infoSlot" name="logInPassword" id="logInPassword"><br>
+        <button type="button" class="otherButton" id="logInSubmit">Log-In</button><br>
+        <a href="javascript:void(0)" class="otherButton" onclick = "document.getElementById('subs1').style.display='block';document.getElementById('fade').style.display='block'">Register</a>
+    </form>
+
+    <!--* register form *-->
+    <div id="subs1" class="white_content">
+        <h3>Register</h3><img src="pictures/DW-logo.png">
+        <div id="regDiv">
+        <form id="register">
+            <label for="Username">Username: </label>
+            <input type="text" class="infoSlot" name="Username" id="Username"><br>
+
+            <label for="Password">Password: </label>
+            <input type="password" class="infoSlot" name="Password" id="Password"><br>
+
+            <label for="confirmPassword">Confirm Password: </label>
+            <input type="password" class="infoSlot" name="confirmPassword" id="confirmPassword"><br>
+
+            <label for="dateOfBirth">Date of Birth: </label>
+            <input type="date" class="infoSlot" name="dateOfBirth" id="dateOfBirth"><br>
+
+            <label for="email">Email: </label>
+            <input type="text" class="infoSlot" name="email" id="email"><br>
+
+            <label for="address">Address: </label>
+            <input type="text" class="infoSlot" name="address" id="address"><br>
+
+            <label for="city">City: </label>
+            <input type="text" class="infoSlot" name="city" id="city"><br>
+
+            <label for="state">State: </label>
+            <select  class="infoSlot" id="state">
+                <option>Not Applicable</option>
+                <option>AL</option>
+                <option>AK</option>
+                <option>AR</option>
+                <option>AZ</option>
+                <option>CA</option>
+                <option>CO</option>
+                <option>CT</option>
+                <option>DE</option>
+                <option>FL</option>
+                <option>GA</option>
+                <option>IA</option>
+                <option>ID</option>
+                <option>IL</option>
+                <option>IN</option>
+                <option>KS</option>
+                <option>KY</option>
+                <option>LA</option>
+                <option>MA</option>
+                <option>MD</option>
+                <option>ME</option>
+                <option>MI</option>
+                <option>MN</option>
+                <option>MO</option>
+                <option>MS</option>
+                <option>MT</option>
+                <option>NC</option>
+                <option>ND</option>
+                <option>NE</option>
+                <option>NH</option>
+                <option>NJ</option>
+                <option>NM</option>
+                <option>NV</option>
+                <option>NY</option>
+                <option>OH</option>
+                <option>OK</option>
+                <option>OR</option>
+                <option>PA</option>
+                <option>RI</option>
+                <option>SC</option>
+                <option>SD</option>
+                <option>TN</option>
+                <option>TX</option>
+                <option>UT</option>
+                <option>VA</option>
+                <option>VT</option>
+                <option>WA</option>
+                <option>WI</option>
+                <option>WV</option>
+                <option>WY</option>
+            </select><br>
+
+            <label for="country">Country: </label>
+            <select  class="infoSlot" id="country">
+                <option>United States</option>
+                <option>Canada</option>
+                <option>Ireland</option>
+                <option>England</option>
+                <option>Japan</option>
+                <option>Australia</option>
+                <option>New Zealand</option>
+                <option>Mexico</option>
+                <option>France</option>
+                <option>Italy</option>
+            </select><br>
+
+            <label for="zip">Zip Code: </label>
+            <input type="text" class="infoSlot" name="zip" id="zip"><br>
+
+            <label for="credCard">Credit/Debt Card: </label>
+            <input type="text" class="infoSlot" name="credCard" id="credCard"><br>
+            <button type="button" id="submit">Submit</button>
+        </form>
+        </div>
+        <a href = "javascript:void(0)" class="close" onclick = "document.getElementById('subs1').style.display='none';document.getElementById('fade').style.display='none'">x</a>
+    </div>
+    <!--* register form end *-->
+
+    <div id="fade" class="black_overlay"></div>
 </div>
 <!--***  content end  ***-->
 <!--# # # # # # # # # #-->
@@ -49,7 +144,7 @@
     <div id="copyright">
         <p>&copy 2015 - DieWing Inc. All Rights Reserved</p>
     </div>
-    <div id="lowLinks">
+    <div id="signInlowLinks">
         <a href="index.php">Home</a>
         <a href="signIn.php">Sign-In</a>
         <a href="about.php">About Us</a>
