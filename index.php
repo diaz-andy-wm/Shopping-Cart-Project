4981241345d5efebd510864918ff83f0f79df899
Start Coding Student 1 <session1@START-Coding-MBP-09.local>
2016-02-16 15:20:20 -0700

    Homepage Slider

--- index.php
+++ index.php
@@ -39,8 +39,57 @@
 <!--# # # # # # # # # # -->
 <!--#   CONTENT DIV   #-->
 <!--# # # # # # # # # # -->
-<div id="content">
-    <img src="http://yaleherald.com/wp-content/uploads/2011/04/BTbw-550x269.jpg">
+<div class="container">
+    <div id="content-slider">
+        <div id="slider">
+            <div id="mask">
+                <ul>
+                    <li class="firstanimation">
+                        <img class="imgSize" src="http://jorgequeiroz.com/site/wp-content/uploads/2013/01/tie-1000x666.jpg"/>
+                    </li>
+
+                    <li class="secondanimation">
+                        <img class="imgSize" src="https://res.cloudinary.com/trunk-club/image/upload/v1428330508/public_site/blog/2013/01/eg0eH.jpg"/>
+                    </li>
+
+                    <li class="thirdanimation">
+                        <img class="imgSize" src="http://2.bp.blogspot.com/-yHf0reh4_PA/T8Bcz2LupWI/AAAAAAAAAkU/r5LntmNCWhc/s1600/SpencersAtTheWaterfront_BurlingtonWeddingPhotography_VPStudiosPhotography4.jpg"/>
+                    </li>
+
+                    <li class="fourthanimation">
+                        <img class="imgSize" src="http://wallpoper.com/images/00/40/65/67/minimalistic-eleventh_00406567.jpg"/>
+                    </li>
+
+                    <li class="fifthanimation">
+                        <img class="imgSize" src="http://blog.ismaelburciaga.com/wp-content/uploads/2011/08/promss5.jpg"/>
+                    </li>
+                </ul>
+            </div>
+            <ul style="margin-left: 703px">
+                <li class="firstanimation">
+                    <img class="imgSize" src="http://jorgequeiroz.com/site/wp-content/uploads/2013/01/tie-1000x666.jpg"/>
+                </li>
+
+                <li class="secondanimation">
+                    <img class="imgSize" src="https://res.cloudinary.com/trunk-club/image/upload/v1428330508/public_site/blog/2013/01/eg0eH.jpg"/>
+                </li>
+
+                <li class="thirdanimation">
+                    <img class="imgSize" src="http://2.bp.blogspot.com/-yHf0reh4_PA/T8Bcz2LupWI/AAAAAAAAAkU/r5LntmNCWhc/s1600/SpencersAtTheWaterfront_BurlingtonWeddingPhotography_VPStudiosPhotography4.jpg"/>
+                </li>
+
+                <li class="fourthanimation">
+                    <img class="imgSize" src="http://wallpoper.com/images/00/40/65/67/minimalistic-eleventh_00406567.jpg"/>
+                </li>
+
+                <li class="fifthanimation">
+                    <img class="imgSize" src="http://blog.ismaelburciaga.com/wp-content/uploads/2011/08/promss5.jpg"/>
+                </li>
+            </ul>
+        </div>
+            <div class="progress-bar"></div>
+        </div>
+    </div>
 </div>
 <!--***  content end  ***-->
 <!--# # # # # # # # # #-->
