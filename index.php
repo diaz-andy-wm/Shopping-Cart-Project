1919ec64efef6f97f47d6a09bf2a0e5fe607898e
Start Coding Student 1 <session1@START-Coding-MBP-09.local>
2016-02-23 13:17:20 -0700

    commit - later

--- index.php
+++ index.php
@@ -67,23 +67,23 @@
             </div>
             <ul style="margin-left: 703px">
                 <li class="firstanimation">
-                    <img class="imgSize" src="http://jorgequeiroz.com/site/wp-content/uploads/2013/01/tie-1000x666.jpg"/>
+                    <img class="imgSize2" src="http://jorgequeiroz.com/site/wp-content/uploads/2013/01/tie-1000x666.jpg"/>
                 </li>
 
                 <li class="secondanimation">
-                    <img class="imgSize" src="https://res.cloudinary.com/trunk-club/image/upload/v1428330508/public_site/blog/2013/01/eg0eH.jpg"/>
+                    <img class="imgSize2" src="https://res.cloudinary.com/trunk-club/image/upload/v1428330508/public_site/blog/2013/01/eg0eH.jpg"/>
                 </li>
 
                 <li class="thirdanimation">
-                    <img class="imgSize" src="http://2.bp.blogspot.com/-yHf0reh4_PA/T8Bcz2LupWI/AAAAAAAAAkU/r5LntmNCWhc/s1600/SpencersAtTheWaterfront_BurlingtonWeddingPhotography_VPStudiosPhotography4.jpg"/>
+                    <img class="imgSize2" src="http://2.bp.blogspot.com/-yHf0reh4_PA/T8Bcz2LupWI/AAAAAAAAAkU/r5LntmNCWhc/s1600/SpencersAtTheWaterfront_BurlingtonWeddingPhotography_VPStudiosPhotography4.jpg"/>
                 </li>
 
                 <li class="fourthanimation">
-                    <img class="imgSize" src="http://wallpoper.com/images/00/40/65/67/minimalistic-eleventh_00406567.jpg"/>
+                    <img class="imgSize2" src="http://wallpoper.com/images/00/40/65/67/minimalistic-eleventh_00406567.jpg"/>
                 </li>
 
                 <li class="fifthanimation">
-                    <img class="imgSize" src="http://blog.ismaelburciaga.com/wp-content/uploads/2011/08/promss5.jpg"/>
+                    <img class="imgSize2" src="http://blog.ismaelburciaga.com/wp-content/uploads/2011/08/promss5.jpg"/>
                 </li>
             </ul>
         </div>
